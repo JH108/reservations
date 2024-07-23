c6c5b929fe027e026cb1cfd0a1a075fe8f6d0958
Jesse Hill <jessehill108@gmail.com>
2024-07-23 06:08:17 -0500

    Ensure the home route can return json

--- src/main/kotlin/me/jessehill/plugins/Routing.kt
+++ src/main/kotlin/me/jessehill/plugins/Routing.kt
@@ -4,11 +4,14 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(buildJsonObject { put("message", "Hello, World!") })
         }
 
         staticResources("/static", "static")
--- src/test/kotlin/me/jessehill/ApplicationTest.kt
+++ src/test/kotlin/me/jessehill/ApplicationTest.kt
@@ -1,21 +1,24 @@
 package me.jessehill
 
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
-import me.jessehill.plugins.*
+import io.ktor.utils.io.charsets.*
+import me.jessehill.plugins.configureRouting
+import me.jessehill.plugins.configureSerialization
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
+    fun `test that home returns JSON`() = testApplication {
         application {
+            configureSerialization()
             configureRouting()
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals(ContentType.Application.Json.withCharset(Charsets.UTF_8), contentType())
         }
     }
 }
